b0e89614d1ca363052611dab200449922a4c9f39
SsNiPeR1 <63201833+SsNiPeR1@users.noreply.github.com>
2022-10-03 20:24:24 +0200

    Update archsearch.py

--- archsearch.py
+++ archsearch.py
@@ -31,7 +31,7 @@ class ARSMod(loader.Module):
 
     @loader.unrestricted
     async def arvcmd(self, message):
-        """Get the version of a package
+        """Get version of a package
         .arv <package> <repo>"""
         args = utils.get_args(message)
         if await self.allmodules.check_security(message, security.OWNER | security.SUDO):
